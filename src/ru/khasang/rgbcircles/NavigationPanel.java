faec834ba827b0e5d6cd3d1a2196a99fca765c94
Victor-Kul <webdevelopervk@yandex.ru>
2015-11-24 01:50:59 +0500

    1) Создан класс "NavigationPanel" для реализации управляющих кнопок 2) размещены кнопки в верхней панели: `пуск`, `пауза`

---
+++ src/ru/khasang/rgbcircles/NavigationPanel.java
@@ -0,0 +1,54 @@
+package ru.khasang.rgbcircles;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Created by Victor-Kul on 23.11.2015.
+ */
+//public class NavigationPanel extends Activity {
+public class NavigationPanel{
+
+//    private final Activity activity;
+    private ImageButton mStartButton, mCancelButton;
+
+    private Timer mTimer;
+//    private MyTimerTask mMyTimerTask;
+    private GameManager gameManager;
+
+    public NavigationPanel(GameManager _gameManager) {
+//        this.activity = _activity;
+//        this.gameManager = _gameManager;
+
+
+//        mStartButton = (ImageButton) findViewById(R.id.imageButtonPlay);
+//        mCancelButton = (ImageButton) findViewById(R.id.imageButtonPause);
+//        mStartButton = (ImageButton) this.gameManager.canvasView.findViewById(R.id.imageButtonPlay);
+//        mCancelButton = (ImageButton) this.gameManager.canvasView.findViewById(R.id.imageButtonPause);
+//    }
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//    super.onCreate(savedInstanceState);
+//        mCancelButton.setOnClickListener(new View.OnClickListener() {
+//
+//            @Override
+//            public void onClick(View v) {
+//                if (mTimer != null) {
+//                    mTimer.cancel();
+//                    mTimer = null;
+//                }
+//            }
+//        }   );
+
+
+    }
+
+
+}
